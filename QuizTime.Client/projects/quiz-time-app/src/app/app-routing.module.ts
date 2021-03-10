e25f00b3732e0c392af222a7d93f57f7b455301b
Lachezar Georgiev <lgeorgie@progress.com>
2021-03-10 20:13:11 +0200

    feat(client): add styling to home page

--- QuizTime.Client/projects/quiz-time-app/src/app/app-routing.module.ts
+++ QuizTime.Client/projects/quiz-time-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { QuizComponent } from './components/quiz/quiz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'quiz', component: QuizComponent },
+  { path: '**', component: HomeComponent },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
