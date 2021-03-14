7222a794d9428f067e64f62c264b0591f0cfeb28
Lachezar Georgiev <lgeorgie@progress.com>
2021-03-14 14:45:16 +0200

    chore(client): update about page

--- QuizTime.Client/projects/quiz-time-app/src/app/components/about/about.component.ts
+++ QuizTime.Client/projects/quiz-time-app/src/app/components/about/about.component.ts
@@ -4,6 +4,4 @@ import { Component } from '@angular/core';
   selector: 'app-about',
   templateUrl: './about.component.html'
 })
-export class AboutComponent {
-  public repoPage = 'https://github.com/lachezar-georgiev/QuizTime';
-}
+export class AboutComponent {}
